ee21002f4b09dc1d556e93e2c027f5d7fa4b0c97
osmarsoto <osmrsoto@hotmail.com>
2022-10-08 19:37:13 -0300

    inclusão de titulo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ola git hub");
+        System.out.println("aqui eu fiz o segundo commit");
     }
 }
